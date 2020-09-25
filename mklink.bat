56818927cec29ace973ad758945281abc24dbf33
maroron <aadd9e@gmail.com>
2020-09-25 23:00:20 +0900

    first commit

---
+++ mklink.bat
@@ -0,0 +1,6 @@
+rem mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+mklink %HOMEPATH%"\.condarc" %HOMEPATH%"\dotfiles\.condarc"
+rem mklink /D %HOMEPATH%"\.vim\ftdetect" %HOMEPATH%"\dotfiles\ftdetect"
+rem mklink /D %HOMEPATH%"\.vim\indent" %HOMEPATH%"\dotfiles\indent"
+rem mklink /D %HOMEPATH%"\.vim\colors" %HOMEPATH%"\dotfiles\colors"
+exit 0
