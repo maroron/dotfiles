3ba65ce266e84a3e4ef4811fa5d22a4f875d0f0a
maroron <aadd9e@gmail.com>
2020-09-26 23:40:16 +0900

    update unknown

--- mklink.bat
+++ mklink.bat
@@ -1,4 +1,7 @@
 mklink %HOMEPATH%"\_vimrc" %HOMEPATH%"\dotfiles\_vimrc"
 mklink %HOMEPATH%"\_gvimrc" %HOMEPATH%"\dotfiles\_gvimrc"
 mklink %HOMEPATH%"\.condarc" %HOMEPATH%"\dotfiles\.condarc"
+
+mklink %HOMEPATH%"\.vim\rc\dein.toml" %HOMEPATH%"\dotfiles\rc\dein.toml"
+mklink %HOMEPATH%"\.vim\rc\dein_lazy.toml" %HOMEPATH%"\dotfiles\rc\dein_lazy.toml"
 exit 0
